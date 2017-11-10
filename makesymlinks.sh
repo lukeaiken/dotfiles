75b37984ae5ea42ebe5e140346be61020b22fdba
Luke Aiken <lukeaiken@gmail.com>
2017-11-10 16:45:18 -0700

    udpate

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
-files="gitconfig git_template gemrc vimrc zshrc" # list of files/folders to symlink in homedir
+files="tmux.conf gitconfig git_template gemrc vimrc zshrc" # list of files/folders to symlink in homedir
 
 ##########
 
